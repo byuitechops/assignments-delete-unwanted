e731203d994cb17d5adc5d2b132f5a62c0c6b7fe
Danverde <gre12014@byui.edu>
2018-01-10 08:59:29 -0700

    should be working

--- main.js
+++ main.js
@@ -3,20 +3,50 @@
 /* Module Description */
 
 /* Put dependencies here */
- const canvas = require('canvas-wrapper');
+const canvas = require('canvas-wrapper'),
+    asyncLib = require('async');
 
 
 module.exports = (course, stepCallback) => {
-    course.addModuleReport('assignments-remove-unwanted');
+    /* array of regEx assignment types to delete */
+    var tests = [
+        /\[co~\d*\]/i
+    ];
 
-    
-    
-    
-    
-    // course.success('assignments-remove-unwanted', 'assignments-remove-unwanted successfully ...');
+    course.addModuleReport('assignments-delete-unwanted');
 
-    // course.throwErr('assignments-remove-unwanted', e);
+    /******************************
+     * deletes a single assignment
+     ******************************/
+    function deleteAssignment(assignment, cb) {
+        canvas.delete(`/api/v1/courses/${course.info.canvasOU}/assignments/${assignment.id}`, (err) => {
+            if (err) {
+                cb(err);
+                return;
+            }
+            course.success('assignments-delete-unwanted', `assignments-delete-unwanted deleted ${assignment.name}`);
+            cb(null);
+        });
+    }
 
-
-    stepCallback(null, course);
+    /**********************************************
+     * gets all assignments in course, and filters
+     * them according to tests array
+     **********************************************/
+    canvas.getAssignments(course.info.canvasOU, (err, assignments) => {
+        var assignmentsToDelete = assignments.filter((assignment) => {
+            var toDelete = false; //true to delete
+            tests.forEach((test) => {
+                if (test.test(assignment.name))
+                    toDelete = true;
+            });
+            return toDelete;
+        });
+        asyncLib.each(assignmentsToDelete, deleteAssignment, (err) => {
+            if (err) {
+                course.throwErr('assignments-delete-unwanted', err);
+            }
+            stepCallback(null, course);
+        });
+    });
 };
